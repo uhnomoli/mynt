806e672267a500f2ef1f36b56cca4939c1f7d9ef
Anomareh <Zonezero5@gmail.com>
2014-07-24 07:37:27 -0400

    Cleaned up the default theme.

--- mynt/themes/light/_assets/js/_uncompressed/mynt.js
+++ mynt/themes/light/_assets/js/_uncompressed/mynt.js
@@ -1,19 +1,27 @@
-$(window).load(function () {
+$(window).on('load', function () {
     // Preserve vertical rhythm.
-    $('.body img').each(function () {
+    var baseline = 9;
+    
+    $('.post img, div.code').each(function () {
         var
             $this = $(this),
+            $anchor = $this.parent('a'),
             
-            baseline = parseInt($('p').css('line-height'), 10),
-            height = $this.outerHeight(true),
+            margin_top = parseInt($this.css('margin-top'), 10),
+            margin_bottom = parseInt($this.css('margin-bottom'), 10),
             
+            height = $this.outerHeight(),
             remainder = height % baseline;
         
+        if ($anchor.length) {
+            margin_bottom += 1;
+            
+            $anchor.css('border-bottom-width', '0px');
+            $this.css('margin-bottom', margin_bottom);
+        }
+        
         if (remainder) {
-            var
-                difference = baseline - remainder,
-                margin_top = parseInt($this.css('margin-top'), 10),
-                margin_bottom = parseInt($this.css('margin-bottom'), 10);
+            var difference = baseline - remainder;
             
             $this.css('margin-top', margin_top + Math.ceil(difference / 2));
             $this.css('margin-bottom', margin_bottom + Math.floor(difference / 2));
